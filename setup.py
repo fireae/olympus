d405c36541948187baef9f79ec78dbdbe5e2b5b9
Subhash Ramesh <subby@Subhashs-MacBook-Pro.local>
2017-11-19 00:38:55 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,15 @@ setup(
   author_email = 'hello@galiboo.com',
   url = 'https://github.com/galiboo/olympus', # use the URL to the github repo
   download_url = 'https://github.com/galiboo/olympus/archive/0.1.tar.gz', 
+  py_modules=['olympus'],
   keywords = ['machine learning', 'python', 'rest', 'api', 'deep learning'],
   classifiers = [
     'Development Status :: 4 - Beta',
     'Programming Language :: Python'
-  ]
+  ],
+  entry_points={
+  'console_scripts': [
+    'olympus = olympus.__main__:cli'
+    ]
+  },
 )
