dfad0423d38dde0c4a6fe9a05dcadc5c0f48c915
Subhash Ramesh <subby@Subhashs-MacBook-Pro.local>
2017-11-19 00:19:22 -0800

    1st release to PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'olympus',
+  packages = ['olympus'],
+  version = '0.1',
+  description = 'A tool for creating a REST API for any machine learning model, in seconds.',
+  author = 'Galiboo',
+  author_email = 'hello@galiboo.com',
+  url = 'https://github.com/galiboo/olympus', # use the URL to the github repo
+  download_url = 'https://github.com/galiboo/olympus/archive/0.1.tar.gz', 
+  keywords = ['machine learning', 'python', 'rest', 'api', 'deep learning']
+  classifiers = [],
+)
